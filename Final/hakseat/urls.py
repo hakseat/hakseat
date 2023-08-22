95cbcd9b26d5f138789a7147a92b62cc27ccb5e8
somin850 <132176662+somin850@users.noreply.github.com>
2023-08-22 20:23:56 +0900

    hakseat

---
+++ Final/hakseat/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from.views import Sub
+from content.views import Main,UploadFeed
+from.settings import MEDIA_URL,MEDIA_ROOT
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('main/', Main.as_view()),
+    path('content/upload',UploadFeed.as_view()),
+    path('content/',include('content.urls')),#이게 content urls를 불러 온것
+    path('user/',include('user.urls')),
+    path('my_web_project/', include('my_web_project.urls'))
+
+]
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+
